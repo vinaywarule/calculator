37ef9e9f0868e0f47dd1be4388fe5e36371a871d
Vinay Warule <vinaywarule@gmail.com>
2025-05-19 08:49:05 +0000

    Returned zero when input string is zero.

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -2,8 +2,10 @@ require_relative '../string_addition'
 
 RSpec.describe 'StringAddition' do
   describe '#add' do
-    it 'should return 0 for an empty input string' do
-      expect(StringAddition.add('')).to eq(0)
+    context "when an empty input string is pass" do
+      it 'should return 0' do
+        expect(StringAddition.add('')).to eq(0)
+      end
     end
   end
 end
--- string_addition.rb
+++ string_addition.rb
@@ -1,2 +1,5 @@
 class StringAddition
+    def self.add(string_input)
+        return 0 if string_input.empty?
+    end
 end

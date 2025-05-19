556bd5d8ec3ac080c2cafe9074d1ad54dd4577bd
Vinay Warule <vinaywarule@gmail.com>
2025-05-19 09:34:42 +0000

    Added logic to support different delimiters other than comma and new line.

--- string_addition.rb
+++ string_addition.rb
@@ -1,6 +1,13 @@
 class StringAddition
     def self.add(string_input)
         return 0 if string_input.empty?
-        string_input.split(/,|\n/).map(&:to_i).sum
+
+        number_delimiter = /,|\n/
+        if string_input.start_with?('//')
+            other_delimiter_details, string_input = string_input.split("\n", 2)
+            number_delimiter = Regexp.escape(other_delimiter_details[2])
+        end
+
+        string_input.split(number_delimiter).map(&:to_i).sum
     end
 end

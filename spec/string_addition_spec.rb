43ace4455024c1ff65c9d14c64b40f9bd3b40292
Vinay Warule <vinaywarule@gmail.com>
2025-05-19 08:58:16 +0000

    Specs for one input string number.

--- spec/string_addition_spec.rb
+++ spec/string_addition_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe 'StringAddition' do
         expect(StringAddition.add('')).to eq(0)
       end
     end
+
+    context "when 1 numbers is pass" do
+      let(:input) { '2' }  
+      
+      it "should return that number" do
+        expect(StringAddition.add(input)).to eq(2)
+      end
+    end
   end
 end
